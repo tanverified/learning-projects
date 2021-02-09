fe3048f6d84fe6aa37258a174b2630e8500a2aee
tanverified <khantanveer2u@gmail.com>
2021-02-09 21:06:33 +0530

    uploading projects

---
+++ BackgroundGenerator/script.js
@@ -0,0 +1,16 @@
+let outputCss = document.querySelector('h3');
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
+let body = document.querySelector('.gradient');
+
+let setGradient = ()=>{
+    body.style.background = `linear-gradient(to right, ${color1.value} , ${color2.value})`;
+    outputCss.textContent = body.style.background +';';
+}
+
+color1.addEventListener('input',setGradient);
+
+color2.addEventListener('input',setGradient);
+
+
+
