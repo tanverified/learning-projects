5466d3e6b9bbd54de29f49046a8095c2b7927b80
tanverified <khantanveer2u@gmail.com>
2021-04-23 11:33:21 +0530

    destrucring props

--- robofriend/src/Card.js
+++ robofriend/src/Card.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 
 const Card = (props) => {
+    const {name, email, id} = props;
     return ( 
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${props.id}?size=200x200`} alt="robots"/>
+            <img src={`https://robohash.org/${id}?size=200x200`} alt="robots"/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
      );
