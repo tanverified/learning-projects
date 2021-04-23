0fb2ab0cb0455d65359628ea9a13c18e87fbcf16
tanverified <khantanveer2u@gmail.com>
2021-04-23 10:46:43 +0530

    card component

---
+++ robofriend/src/Card.js
@@ -0,0 +1,13 @@
+const Card = () => {
+    return ( 
+        <div>
+            <img src="" alt="robots"/>
+            <div>
+                <h2>Jane doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Card;
--- robofriend/src/index.js
+++ robofriend/src/index.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
+import Card from './Card';
 
-ReactDOM.render(<Hello welcome={'Welcome React Ninjas'} />, document.getElementById('root'));
+ReactDOM.render(<Card />, document.getElementById('root'));
