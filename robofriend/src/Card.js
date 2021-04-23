3013973e6388f815c7cf86e81f1988e25186fcb3
tanverified <khantanveer2u@gmail.com>
2021-04-23 17:32:46 +0530

    adding more robots

--- robofriend/src/Card.js
+++ robofriend/src/Card.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-
 const Card = ({name, email, id}) => {
     
     return ( 
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img src={`https://robohash.org/${id}?size=200x200`} alt="robots"/>
             <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
+              <h2>{name}</h2>
+              <p>{email}</p>
             </div>
         </div>
      );
--- robofriend/src/index.js
+++ robofriend/src/index.js
@@ -1,12 +1,9 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import Card from './Card';
-import {robots} from './robots'
+import CardList from './CardList';
+import {robots} from './robots';
 
-ReactDOM.render(<div>
-                   <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-                   <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-                   <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-                </div>
+ReactDOM.render( <CardList robots={robots}/>
     , document.getElementById('root'));
