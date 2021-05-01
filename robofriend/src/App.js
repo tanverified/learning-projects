37d6e2f65a28df72de02c6e816c653b09295bdb5
tanverified <khantanveer2u@gmail.com>
2021-05-01 16:38:54 +0530

    searchbox component

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -1,9 +1,14 @@
 import CardList from './CardList';
 import {robots} from './robots';
+import Searchbox from './Searchbox';
 
 const App = () => {
     return ( 
-        <CardList robots={robots}/>
+        <div className='tc'>
+          <h1>Robofriends</h1>
+          <Searchbox/>
+          <CardList robots={robots}/>
+        </div>
      );
 }
  
---
+++ robofriend/src/Searchbox.js
@@ -0,0 +1,10 @@
+
+const SearchBox = () => {
+    return ( 
+        <div className='pa2'>
+            <input className='pa3 ba b--green bg-lightest-blue' type="search" placeholder="search robot"/>
+        </div>
+     );
+}
+ 
+export default SearchBox;
