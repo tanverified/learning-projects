18ea060a1e05ea6bf01cc741929e2c282da38db0
tanverified <khantanveer2u@gmail.com>
2021-06-20 20:24:21 +0530

    done

--- digital-clock-react/src/App.js
+++ digital-clock-react/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let time = new Date().toLocaleTimeString();
+
+  const [currentTime, setCurrentTime] = useState(time);
+
+  setInterval(() => {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="clock">{currentTime}</div>
+    </>
   );
-}
+};
 
 export default App;
